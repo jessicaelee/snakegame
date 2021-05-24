cd95c607fba39c42539beefc81252b43ac82f4e8
jessicaelee <jessica.e.lee.93@gmail.com>
2021-05-24 13:17:45 -0700

    snake game

---
+++ game.js
@@ -0,0 +1,45 @@
+import { update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection, SNAKE_SPEED } from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js"
+let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board");
+let gameOver = false;
+
+// checks how long before we need to render the game
+// SNAKE_SPEED determines speed that we render it
+function main(currentTime) {
+
+  if (gameOver) {
+    if (confirm("You lost. Press ok to restart.")) {
+      window.location = "/"
+    }
+    return;
+  }
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+  updateSnake()
+  updateFood()
+  checkForDeath();
+}
+
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkForDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
